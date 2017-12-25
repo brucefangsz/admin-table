faf681f9e2b282317d8a2f6c2774d31dd3e73234
shuaizhong <fangshzho@yonyou.com>
2017-12-25 12:49:50 +0800

    up

--- src/pages/main/component/icon.js
+++ src/pages/main/component/icon.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'React';
+import React, { Component } from 'react';
 import { Icon } from 'tinper-bee';
 export default class Logo extends Component {
     constructor() {
